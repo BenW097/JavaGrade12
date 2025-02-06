9695ac06f533519e4096e68be9dc1f2ca42eb0c5
Ben <339710097@gotvdsb.ca>
2025-02-06 12:23:53 -0500

    l3

---
+++ Lesson_4_review/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.Random;
+import java.lang.String;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Q1
+        /*
+        Scanner input= new Scanner(System.in);
+        System.out.println("Enter a username: ");
+        String userName = input.next();
+        userName = userName.toUpperCase();
+        while(true){
+            System.out.println("Enter a password that is at least 8 characters: ");
+            String password = input.next();
+            int plength = password.length();
+            if (plength >= 8) {
+                password = password.toLowerCase();
+                System.out.println("Your username is "+userName+" and your password is "+password);
+                break;
+            }
+        }
+
+         */
+
+        //Q2
+
+    }
+}
